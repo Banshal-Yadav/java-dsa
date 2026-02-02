626b12f62243a1f1f5bc8b028f431bda4d098106
banshal-Yadav <banshalyadav99@gmail.com>
2026-02-02 23:37:19 +0530

    feat: Qs.1672 - Richest customer wealth problem on Leetcode

---
+++ 07-Problems/Leetcode/Arrays/MaxWealth.java
@@ -0,0 +1,24 @@
+// Qs. 1672
+package Leetcode.Arrays;
+
+public class MaxWealth {
+    public int maximumWealth(int[][] accounts) {
+        int cuu = 0;
+        int maxWealth = 0;
+        // rows loop
+        for(int row = 0; row < accounts.length; row++){
+            // resets current wealth sum every row
+            cuu = 0;
+            // cols loop
+            for(int col = 0; col < accounts[row].length; col++) {
+                // sum of wealth in single row
+                cuu += accounts[row][col];
+            }
+            // update max sum every rows loop
+            if(cuu > maxWealth) {
+                maxWealth = cuu;
+            }
+        }
+        return maxWealth;
+    }
+}
