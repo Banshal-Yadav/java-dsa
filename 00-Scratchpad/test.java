ffee01911e7a0d99e5e1ed7e23fdabecb41547fb
banshal-Yadav <banshalyadav99@gmail.com>
2026-04-14 19:48:54 +0530

    minor changes

--- 00-Scratchpad/test.java
+++ 00-Scratchpad/test.java
@@ -1,5 +1,5 @@
 // 2d array
-
+import java.util.*;
 import java.util.Scanner;
 
 class test {
@@ -10,10 +10,18 @@ class test {
 
         System.out.println("Length of the 2d array is:" + arr.length);
 
+        // input
         for(int row = 0; row < arr.length ; row++) {
             for(int col = 0; col < arr[row].length; col++) {
                 arr[row][col] = sc.nextInt();
             }
         }
+
+        // output
+        for (int row = 0; row < arr.length; row++) {
+            System.out.println(Arrays.toString(arr[row]));
+        }
+
+        sc.close();
     }
 }
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Data Structures and Algorithms practice in Java.
 ## Topics Covered
 - Arrays
 - 2D Arrays
-- 
+- Strings
 
 ( Will update as i go )
